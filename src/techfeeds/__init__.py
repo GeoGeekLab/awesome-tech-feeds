7f558ff8f8f59589ec6c91c6df5febf1d937810b
GeoGeek <huazaiheng7@gmail.com>
2026-09-20 14:51:18 +0800

    feat: route CLI and compatibility exports through consumer SDK

--- src/techfeeds/__init__.py
+++ src/techfeeds/__init__.py
@@ -1,3 +1,32 @@
-"""Awesome Tech Feeds registry tooling."""
+"""Awesome Tech Feeds registry tooling and public consumer SDK."""
 
-__version__ = "0.3.0"
+from .consumer import (
+    DEFAULT_REGISTRY_URL,
+    ConsumerError,
+    Query,
+    QueryContractError,
+    QueryResult,
+    Registry,
+    RegistryCompatibilityError,
+    RegistryIntegrityError,
+    RegistryLoadError,
+    SourceRecord,
+    UnknownFilterValueError,
+)
+
+__version__ = "0.4.0"
+
+__all__ = [
+    "DEFAULT_REGISTRY_URL",
+    "ConsumerError",
+    "Query",
+    "QueryContractError",
+    "QueryResult",
+    "Registry",
+    "RegistryCompatibilityError",
+    "RegistryIntegrityError",
+    "RegistryLoadError",
+    "SourceRecord",
+    "UnknownFilterValueError",
+    "__version__",
+]
